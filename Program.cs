52d0ac2513d120060b8b294ee8887a2a0516576c
gothwavy <ryanalmeidadev@gmail.com>
2024-12-22 18:00:38 -0300

    Finalizado✅

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace _06_Encapsulamento
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno Ryan = new Aluno();
+            Ryan.Mensagem();
+            // O objeto nao precisa acessar as informacoes dos atributos ou utilizar o metodo media diretamente, entao ao usar private, restringimos o acesso do objeto a fim de deixar o codigo mais intuivo.
+        }
+    }
+}
